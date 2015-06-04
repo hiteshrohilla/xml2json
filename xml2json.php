c124d24ae80075e940741182be6a9b579ca6373b
Hitesh Kumar <rohilla.hitesh@gmail.com>
2015-06-04 21:31:16 +0530

    Update xml2json.php

--- xml2json.php
+++ xml2json.php
@@ -9,21 +9,21 @@
 
 		    // Error handeling for URL correction
 		    if($fileContents === FALSE) {
-		    	echo 404;	// of XML at URL is not available
+		    	echo 404;	// if XML File or URL is not available
 		    } else {
 		    	// of XML at URL is available
 		    	$fileContents = str_replace(array("\n", "\r", "\t"), '', $fileContents); // sanitise xml
-			    $fileContents = trim(str_replace('"', "'", $fileContents)); // sanitise xml
-			    $simpleXml = simplexml_load_string($fileContents); // load xml as string
-			    $json = json_encode($simpleXml); // convert xml to json
-			    echo $json; // return json
+			$fileContents = trim(str_replace('"', "'", $fileContents)); // sanitise xml
+			$simpleXml = simplexml_load_string($fileContents); // load xml as string
+			$json = json_encode($simpleXml); // convert xml to json
+			echo $json; // return json
 
-			    //CODE TO SAVE XML INTO a JSON FILE
-			    $json_parts = pathinfo($url);
-				$json_file = $json_parts['filename'] . '.json'; // since PHP 5.2.0
-				$myfile = fopen($json_file, "w") or die("Unable to open file!");
-				fwrite($myfile, $json);
-				fclose($myfile);
+			//CODE TO SAVE XML INTO a JSON FILE
+			$json_parts = pathinfo($url);
+			$json_file = $json_parts['filename'] . '.json'; // since PHP 5.2.0
+			$myfile = fopen($json_file, "w") or die("Unable to open file!");
+			fwrite($myfile, $json);
+			fclose($myfile);
 		    }
 		} else {
 			echo "Err!"; // in case someone try to access the script will get this.
